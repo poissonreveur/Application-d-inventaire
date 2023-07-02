6a2399860a65077e7dd09af55e17d20758f6bc4f
poissonreveur <138327365+poissonreveur@users.noreply.github.com>
2023-07-02 10:02:34 +0200

    Update Recenseur.py

--- Recenseur.py
+++ Recenseur.py
@@ -10,7 +10,11 @@ def ajouter_outil():
     date_depot = entry_depot.get()
     etat_materiel = var_etat.get()
     emprunteur = entry_emprunteur.get()
-
+        
+    if not nom_outil:
+        messagebox.showerror("Erreur", "Veuillez renseigner le nom de l'outil.")
+        return
+    
     # Formater les dates
     try:
         if date_emprunt:
@@ -95,61 +99,6 @@ def rechercher():
     if not outil_present:
         messagebox.showinfo("Information", "L'outil n'est pas présent dans le fichier.")
 
-
-import tkinter as tk
-from tkinter import messagebox, StringVar
-from openpyxl import load_workbook
-from datetime import datetime
-
-def ajouter_outil():
-    nom_outil = entry_nom.get()
-    date_emprunt = entry_emprunt.get()
-    date_depot = entry_depot.get()
-    etat_materiel = var_etat.get()
-    emprunteur = entry_emprunteur.get()
-
-    # Formater les dates
-    try:
-        if date_emprunt:
-            #feuille.cell(row=prochaine_ligne, column=2, value=datetime.strptime(date_emprunt, "%d/%m/%Y").strftime("%d/%m/%Y"))
-            date_emprunt = datetime.strptime(date_emprunt, "%d/%m/%Y").strftime("%d/%m/%Y")
-        date_depot = datetime.strptime(date_depot, "%d/%m/%Y").strftime("%d/%m/%Y")
-    except ValueError:
-        messagebox.showerror("Erreur", "Veuillez entrer les dates au format dd/mm/yyyy.")
-        return
-
-    # Charger le fichier Excel existant
-    try:
-        classeur = load_workbook("fichier.xlsx")
-        feuille = classeur.active
-    except:
-        messagebox.showerror("Erreur", "Erreur lors du chargement du fichier Excel.")
-        return
-
-    # Trouver la première ligne vide
-    prochaine_ligne = feuille.max_row + 1
-
-    # Copier les données existantes à partir du fichier Excel
-    for colonne in range(1, 6):
-        cellule_source = feuille.cell(row=1, column=colonne)
-        cellule_destination = feuille.cell(row=prochaine_ligne, column=colonne)
-        cellule_destination.value = cellule_source.value
-
-    # Ajouter les nouvelles informations dans la ligne suivante
-    feuille.cell(row=prochaine_ligne, column=1, value=nom_outil)
-    feuille.cell(row=prochaine_ligne, column=2, value=date_emprunt)
-    feuille.cell(row=prochaine_ligne, column=3, value=date_depot)
-    feuille.cell(row=prochaine_ligne, column=4, value=etat_materiel)
-    feuille.cell(row=prochaine_ligne, column=5, value=emprunteur)
-
-    # Sauvegarder le fichier Excel
-    classeur.save("fichier.xlsx")
-
-    # Effacer les champs après avoir ajouté les informations
-    entry_nom.delete(0, tk.END)
-    entry_emprunt.delete(0, tk.END)
-    entry_depot.delete(0, tk.END)
-    entry_emprunteur.delete(0, tk.END)
 
 def rechercher():
     recherche = entry_recherche.get().lower()
@@ -198,6 +147,9 @@ def ouvrir_fenetre_modifier():
     fenetre_modifier = tk.Toplevel(fenetre)
     fenetre_modifier.title("Modifier un outil")
     fenetre_modifier.configure(background='#2D2D36')
+    fenetre_modifier.iconbitmap('icone.ico')
+    fenetre_modifier.geometry("300x300")
+
 
     def obtenir_informations():
         # Charger le fichier Excel existant
@@ -316,20 +268,33 @@ def ouvrir_fenetre_modifier():
     bouton_annuler = tk.Button(fenetre_modifier, text="Annuler", command=fenetre_modifier.destroy, background='#E04750', fg='white')
     bouton_annuler.pack()
 
+def valider_champs():
+    nom_outil = entry_nom.get()
+    date_depot = entry_depot.get()
+
+    if nom_outil and date_depot:
+        bouton_ajouter.configure(state=tk.NORMAL)
+    else:
+        bouton_ajouter.configure(state=tk.DISABLED)
+
+
 # Création de la fenêtre principale
 fenetre = tk.Tk()
 fenetre.title("Recensement des Outils")
+fenetre.iconbitmap('icone.ico')
 fenetre.configure(background='#2D2D36')
 
 # Création des étiquettes et des champs de saisie
 label_nom = tk.Label(fenetre, text="Nom de l'outil:", background='#2D2D36', fg='white')
 entry_nom = tk.Entry(fenetre)
+entry_nom.bind("<<Modified>>", lambda event: valider_champs())
 
 label_emprunt = tk.Label(fenetre, text="Date d'emprunt:", background='#2D2D36', fg='white')
 entry_emprunt = tk.Entry(fenetre)
 
 label_depot = tk.Label(fenetre, text="Date de dépôt:", background='#2D2D36', fg='white')
 entry_depot = tk.Entry(fenetre)
+entry_depot.bind("<<Modified>>", lambda event: valider_champs())
 
 label_emprunteur = tk.Label(fenetre, text="Emprunteur:", background='#2D2D36', fg='white')
 entry_emprunteur = tk.Entry(fenetre)
@@ -373,6 +338,8 @@ menu_etat.config(background='#2D2D36', fg='white', highlightthickness=0)
 menu_etat.pack()
 
 bouton_ajouter.pack(side=tk.BOTTOM)
+bouton_ajouter = tk.Button(fenetre, text="Ajouter l'outil", command=ajouter_outil, background='#E04750', fg='white')
+bouton_ajouter.configure(state=tk.DISABLED)
 
 # Bouton pour ouvrir la fenêtre de modification
 bouton_modifier = tk.Button(fenetre, text="Modifier un outil", command=ouvrir_fenetre_modifier, background='#E04750', fg='white')
